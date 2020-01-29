a7fba695081ca6d4e081e6de93ec0abe5f82dc8e
Alessio Coser <alessio.coser@xpeppers.com>
2020-01-29 14:19:29 +0100

    fixes publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 TAG=$1
 
-sed -ie "s|'com.github.DaikonWeb:daikon:.*'|'com.github.DaikonWeb:daikon:${TAG}'|g" README.md
-sed -ie "s|<version>.*</version>|<version>${TAG}</version>|g" README.md
+sed -i -e "s|'com.github.DaikonWeb:daikon:.*'|'com.github.DaikonWeb:daikon:${TAG}'|g" README.md
+sed -i -e "s|<version>.*</version>|<version>${TAG}</version>|g" README.md
 
 git commit -am "Release ${TAG}"
 git tag $TAG
