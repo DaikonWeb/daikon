490f49d9b70c5bf2ad64e997a5b3beedeb852bf1
Alessio Coser <alessio.coser@xpeppers.com>
2020-04-01 09:41:08 +0200

    removed backup files

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 TAG=$1
 
-sed -i -e "s|'com.github.DaikonWeb:daikon:.*'|'com.github.DaikonWeb:daikon:${TAG}'|g" README.md
-sed -i -e "s|<version>.*</version>|<version>${TAG}</version>|g" README.md
+sed -i -e "s|'com.github.DaikonWeb:daikon:.*'|'com.github.DaikonWeb:daikon:${TAG}'|g" README.md && rm README.md-e
+sed -i -e "s|<version>.*</version>|<version>${TAG}</version>|g" README.md && rm README.md-e
 
 git commit -am "Release ${TAG}"
 git tag $TAG
